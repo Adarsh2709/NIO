f2ca9175169c5871f50b6eed068e259e42461afa
Adarsh2709 <jhaadarsh709@gmail.com>
2026-03-17 13:39:04 +0530

    feat: add event-driven NIO server runtime with core module and main bootstrap entry

---
+++ Core/src/Server.java
@@ -0,0 +1,34 @@
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+import pipeline.HandlerPipeline;
+import pipeline.LoggingHandler;
+import pipeline.RouterHandler;
+
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.Selector;
+
+public class Server {
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void start()throws Exception{
+        Selector sl = Selector.open();
+
+        ServerSocketChannel server = ServerSocketChannel.open();
+        server.configureBlocking(false);
+        server.bind(new InetSocketAddress(port));
+        server.register(sl, server.validOps()); // Register the channel with the server’s selector so it can listen and react to I/O events.
+
+        HandlerPipeline pipeline = new HandlerPipeline();
+        pipeline.addLast(new LoggingHandler());
+        pipeline.addLast(new RouterHandler());
+
+        pipeline.addLast(new LoggingHandler());
+        pipeline.addLast(new RouterHandler());
+    }
+}
